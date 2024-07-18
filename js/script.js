e0ff72fbef46ff53d92f4b88863420f70c228617
sajib hasan <sajibhasan800080@gmail.com>
2024-07-18 07:11:05 +0600

    added data saving functionality

--- js/script.js
+++ js/script.js
@@ -7,11 +7,6 @@ let item_num = 0;
 
 
 
-
-
-
-
-
 input_box.addEventListener('keydown', (e) => {
     if(e.key == "Enter"){
         add_fun();
@@ -19,6 +14,7 @@ input_box.addEventListener('keydown', (e) => {
 });
 add_btn.addEventListener('click', add_fun);
 reset_btn.addEventListener('click', reset_fun);
+item_field.addEventListener('click', dlt_fun);
 
 
 
@@ -30,37 +26,9 @@ function add_fun(){
 
         if(input_box.value == ''){
 
-            let warning_msg = document.createElement('p');
-            warning_msg.class = "warning-text";
-            warning_msg.style = "background-color : lightyellow; color : black;";
-            let msg_text = document.createTextNode('type something');
-            warning_msg.appendChild(msg_text);
-
-            let warning_text = document.getElementsByClassName('warning-text');
-            for(let i = 0; i < item_field.children.length; i++){
-                for(let j = 0; j < item_field.children.length; j++){
-                    if(item_field.children[i] == warning_text[j]){
-                        item_field.removeChild(item_field.children[i]);
-                    }
-                }
-            }
-
-            item_field.insertAdjacentHTML('beforeend', warning_msg.textContent);
-
+            alert('Text Field in empty!!');
 
         }else{
-
-
-
-            let warning_text = document.getElementsByClassName('warning-text');
-            for(let i = 0; i < item_field.children.length; i++){
-                for(let j = 0; j < item_field.children.length; j++){
-                    if(item_field.children[i] == warning_text[j]){
-                        item_field.removeChild(item_field.children[i]);
-                    }
-                }
-            }
-
 
             
             let element = document.createElement('p');
@@ -82,11 +50,8 @@ function add_fun(){
             edit_btn.appendChild(edit_text);
 
             
-            dlt_btn.addEventListener('click', dlt_fun);
+            
             edit_btn.addEventListener('click', edit_fun);
-
-            
-            
 
             
 
@@ -97,10 +62,6 @@ function add_fun(){
 
             input_box.value = "";
             input_box.focus();
-
-
-
-
 
 
             function edit_fun(){
@@ -141,6 +102,10 @@ function add_fun(){
 
 
                         cancel_btn.addEventListener('click', cancel_fun);
+                        edit_input.addEventListener('blur', blur_fun);
+                        function blur_fun(){
+                            cancel_fun();
+                        }
                         edit_input.addEventListener('keydown', (e) => {
                             if(e.key == "Enter"){
                                 save_fun();
@@ -152,10 +117,6 @@ function add_fun(){
                                 save_fun();
                             }
                         });
-                        edit_input.addEventListener('blur', blur_fun);
-                        function blur_fun(){
-                            cancel_fun();
-                        }
 
 
 
@@ -180,33 +141,174 @@ function add_fun(){
                                     label.replaceChild(new_node, label.childNodes[0]);
                                     
                                     item_field.children[i].replaceChild(label, item_field.children[i].children[1]);
+
+                                    save_data();
                                 }
                             }
-                            
                         }
                     }
                 }
             }
 
-            function dlt_fun(){
-                for(let i = 0; i < item_field.children.length; i++){
-                    if(dlt_btn.parentElement == item_field.children[i]){
-                        item_field.removeChild(item_field.children[i]);
-                    }
-                }
-            }
-
-
             item_num++;
-            }
 
-
-
+            save_data();
+        }
     }
+
+
+
+
+
+
+
 
 
 function reset_fun(){
     item_field.innerHTML = "";
     input_box.focus();
     item_num = 0;
+    console.clear();
+    save_data();
+}
+
+
+let item_selector = 0;
+
+
+function dlt_fun(e){
+    if(e.target.innerHTML == "delete"){
+        for(let i = 0; i < item_field.children.length; i++){
+            if(e.target.parentElement == item_field.children[i]){
+                item_field.removeChild(item_field.children[i]);
+                save_data();
+                // console.log(e.target.innerHTML);
+            }
+        }
+    }else if(e.target.innerHTML == "edit"){
+        for(let i = 0; i < item_field.children.length; i++){
+            if(e.target.classList[0] == `edit${i}`){
+                item_selector = i;
+                external_edit_fun();
+            }
+        }
+    }
+}
+
+
+
+function external_edit_fun(){
+
+    let edit_input = document.createElement('input');
+    edit_input.type = "text";
+    edit_input.placeholder = "type here";
+    edit_input.classList.add('updation-input');
+
+
+    for(let i = 0; i < item_field.children.length; i++){
+
+    }
+    
+
+
+    let element = item_field.children[item_selector];
+    let label = item_field.children[item_selector].children[1];
+    let edit_btn = item_field.children[item_selector].children[2];
+
+    for(let i = 0; i < item_field.children.length; i++){
+
+        
+        
+        if(item_field.children[i] == edit_btn.parentElement){
+
+            let item_value = edit_btn.parentElement.childNodes[1].textContent;
+            console.log(item_value);
+
+            let edit_field = document.createElement('p');
+            edit_field.appendChild(edit_input);
+
+            let cancel_btn = document.createElement('button');
+            let cancel_text = document.createTextNode('cancel');
+            cancel_btn.appendChild(cancel_text);
+            let save_btn = document.createElement('button');
+            let save_text = document.createTextNode('save');
+            save_btn.appendChild(save_text);
+            edit_field.insertAdjacentElement('beforeend', cancel_btn);
+            edit_field.insertAdjacentElement('beforeend', save_btn);
+            
+            
+            item_field.replaceChild(edit_field, item_field.children[i]);
+            edit_input.value = edit_btn.parentElement.childNodes[1].textContent;
+            
+            
+
+            edit_input.focus();
+
+
+
+
+            cancel_btn.addEventListener('click', cancel_fun);
+            edit_input.addEventListener('blur', blur_fun);
+            function blur_fun(){
+                cancel_fun();
+            }
+            edit_input.addEventListener('keydown', (e) => {
+                if(e.key == "Enter"){
+                    save_fun();
+                }
+            });
+            // save_btn.addEventListener('click', save_fun);
+            save_btn.addEventListener('mousedown', (e) => {
+                if(e.button == 0){
+                    save_fun();
+                }
+            });
+
+
+
+            
+            function cancel_fun(){
+                for(let i = 0; i < item_field.children.length; i++){
+                    if(item_field.children[i] == cancel_btn.parentElement){
+                        item_field.replaceChild(element, item_field.children[i]);
+                    }
+                }
+            }
+
+            function save_fun(){
+                edit_input.removeEventListener('blur', blur_fun);
+                for(let i = 0; i < item_field.children.length; i++){
+                    if(item_field.children[i] == save_btn.parentElement){
+                        
+
+                        item_field.replaceChild(element, item_field.children[i]);
+                        
+                        let new_node = document.createTextNode(edit_input.value);
+                        label.replaceChild(new_node, label.childNodes[0]);
+                        
+                        item_field.children[i].replaceChild(label, item_field.children[i].children[1]);
+
+                        save_data();
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+function save_data(){
+    localStorage.setItem('data', item_field.innerHTML);
+}
+function show_data(){
+    item_field.innerHTML = localStorage.getItem('data');
+}
+show_data();
+
+
+for(let i = 0; i < item_field.children.length; i++){
+    item_field.children[i].children[2].classList.add(`edit${i}`);
+    if(item_field.children[i].children[2].innerHTML == 'edit'){
+        item_field.children[i].children[2].addEventListener('click', external_edit_fun);
+    }
 }
