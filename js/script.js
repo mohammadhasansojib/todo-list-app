9d96b241a73fd4ff009251a56fe63d133e0bf6a6
sajib hasan <sajibhasan800080@gmail.com>
2024-07-30 10:27:04 +0600

    fixed function's bugs and added styles with css

--- js/script.js
+++ js/script.js
@@ -24,12 +24,11 @@ item_field.addEventListener('click', dlt_fun);
 
 function add_fun(){
 
-        if(input_box.value == ''){
+        if(input_box.value.trim() == ''){
 
             alert('Text Field in empty!!');
 
         }else{
-
             
             let element = document.createElement('p');
             element.classList.add(`item`, `item${item_num}`);
@@ -42,18 +41,12 @@ function add_fun(){
             let edit_btn = document.createElement('button');
             let edit_text = document.createTextNode('edit');
 
-
-
             label.appendChild(text);
             element.appendChild(label);
             dlt_btn.appendChild(dlt_text);
             edit_btn.appendChild(edit_text);
-
-            
             
             edit_btn.addEventListener('click', edit_fun);
-
-            
 
             element.insertAdjacentElement('afterbegin', checkbox);
             element.insertAdjacentElement('beforeend', edit_btn);
@@ -63,93 +56,6 @@ function add_fun(){
             input_box.value = "";
             input_box.focus();
 
-
-            function edit_fun(){
-
-                let edit_input = document.createElement('input');
-                edit_input.type = "text";
-                edit_input.placeholder = "type here";
-                edit_input.classList.add('updation-input');
-
-
-                for(let i = 0; i < item_field.children.length; i++){
-                    if(item_field.children[i] == edit_btn.parentElement){
-
-                        let item_value = edit_btn.parentElement.childNodes[1].textContent;
-                        console.log(item_value);
-
-                        let edit_field = document.createElement('p');
-                        edit_field.appendChild(edit_input);
-
-                        let cancel_btn = document.createElement('button');
-                        let cancel_text = document.createTextNode('cancel');
-                        cancel_btn.appendChild(cancel_text);
-                        let save_btn = document.createElement('button');
-                        let save_text = document.createTextNode('save');
-                        save_btn.appendChild(save_text);
-                        edit_field.insertAdjacentElement('beforeend', cancel_btn);
-                        edit_field.insertAdjacentElement('beforeend', save_btn);
-                        
-                        
-                        item_field.replaceChild(edit_field, item_field.children[i]);
-                        edit_input.value = edit_btn.parentElement.childNodes[1].textContent;
-                        
-                        
-
-                        edit_input.focus();
-
-
-
-
-                        cancel_btn.addEventListener('click', cancel_fun);
-                        edit_input.addEventListener('blur', blur_fun);
-                        function blur_fun(){
-                            cancel_fun();
-                        }
-                        edit_input.addEventListener('keydown', (e) => {
-                            if(e.key == "Enter"){
-                                save_fun();
-                            }
-                        });
-                        // save_btn.addEventListener('click', save_fun);
-                        save_btn.addEventListener('mousedown', (e) => {
-                            if(e.button == 0){
-                                save_fun();
-                            }
-                        });
-
-
-
-                        
-                        function cancel_fun(){
-                            for(let i = 0; i < item_field.children.length; i++){
-                                if(item_field.children[i] == cancel_btn.parentElement){
-                                    item_field.replaceChild(element, item_field.children[i]);
-                                }
-                            }
-                        }
-
-                        function save_fun(){
-                            edit_input.removeEventListener('blur', blur_fun);
-                            for(let i = 0; i < item_field.children.length; i++){
-                                if(item_field.children[i] == save_btn.parentElement){
-                                    
-
-                                    item_field.replaceChild(element, item_field.children[i]);
-                                    
-                                    let new_node = document.createTextNode(edit_input.value);
-                                    label.replaceChild(new_node, label.childNodes[0]);
-                                    
-                                    item_field.children[i].replaceChild(label, item_field.children[i].children[1]);
-
-                                    save_data();
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
             item_num++;
 
             save_data();
@@ -157,11 +63,96 @@ function add_fun(){
     }
 
 
+function edit_fun(e){
+
+    if(e.target.innerText == 'Edit' && e.target.tagName == 'BUTTON'){
+        let edit_input = document.createElement('input');
+        edit_input.type = "text";
+        edit_input.placeholder = "type here";
+        edit_input.classList.add('updation-input');
+        let prev_element = e.target.parentElement;
+
+
+        for(let i = 0; i < item_field.children.length; i++){
+            if(item_field.children[i] == e.target.parentElement){
+
+                let item_value = e.target.parentElement.childNodes[1].textContent;
+                console.log(item_value);
+
+                let edit_field = document.createElement('p');
+                edit_field.appendChild(edit_input);
+
+                let cancel_btn = document.createElement('button');
+                let cancel_text = document.createTextNode('cancel');
+                cancel_btn.appendChild(cancel_text);
+                let save_btn = document.createElement('button');
+                let save_text = document.createTextNode('save');
+                save_btn.appendChild(save_text);
+                edit_field.insertAdjacentElement('beforeend', cancel_btn);
+                edit_field.insertAdjacentElement('beforeend', save_btn);
+                
+                
+                item_field.replaceChild(edit_field, item_field.children[i]);
+                edit_input.value = e.target.parentElement.childNodes[1].textContent;
+                
+                edit_input.focus();
+
+                cancel_btn.addEventListener('click', blur_fun);
+                edit_input.addEventListener('blur', blur_fun);
+                function blur_fun(e){
+                    cancel_fun(e, prev_element);
+                }
+                edit_input.addEventListener('keydown', (e) => {
+                    if(e.key == "Enter"){
+                        edit_input.removeEventListener('blur', blur_fun);
+                        save_fun(edit_input, save_btn, prev_element);
+                    }
+                });
+                // save_btn.addEventListener('click', save_fun);
+                save_btn.addEventListener('mousedown', (e) => {
+                    if(e.button == 0){
+                        edit_input.removeEventListener('blur', blur_fun);
+                        save_fun(edit_input, save_btn, prev_element);
+                    }
+                });
 
 
 
 
+            }
+        }
+    }
 
+}
+
+
+
+
+function cancel_fun(e, prev_element){
+    for(let i = 0; i < item_field.children.length; i++){
+        if(item_field.children[i] == e.target.parentElement){
+            item_field.replaceChild(prev_element, item_field.children[i]);
+        }
+    }
+}
+
+function save_fun(edit_input, save_btn, prev_element){
+    
+    for(let i = 0; i < item_field.children.length; i++){
+        if(item_field.children[i] == save_btn.parentElement){
+
+            item_field.replaceChild(prev_element, item_field.children[i]);
+
+            let new_node = document.createTextNode(edit_input.value);
+            let label = document.createElement('label');
+            label.appendChild(new_node);
+
+            prev_element.replaceChild(label, item_field.children[i].children[1]);
+
+            save_data();
+        }
+    }
+}
 
 
 function reset_fun(){
@@ -172,125 +163,15 @@ function reset_fun(){
     save_data();
 }
 
-
 let item_selector = 0;
 
-
 function dlt_fun(e){
-    if(e.target.innerHTML == "delete"){
+    if(e.target.innerHTML == "delete" && e.target.tagName == "BUTTON"){
         for(let i = 0; i < item_field.children.length; i++){
             if(e.target.parentElement == item_field.children[i]){
                 item_field.removeChild(item_field.children[i]);
                 save_data();
                 // console.log(e.target.innerHTML);
-            }
-        }
-    }else if(e.target.innerHTML == "edit"){
-        for(let i = 0; i < item_field.children.length; i++){
-            if(e.target.classList[0] == `edit${i}`){
-                item_selector = i;
-                external_edit_fun();
-            }
-        }
-    }
-}
-
-
-
-function external_edit_fun(){
-
-    let edit_input = document.createElement('input');
-    edit_input.type = "text";
-    edit_input.placeholder = "type here";
-    edit_input.classList.add('updation-input');
-
-
-    for(let i = 0; i < item_field.children.length; i++){
-
-    }
-    
-
-
-    let element = item_field.children[item_selector];
-    let label = item_field.children[item_selector].children[1];
-    let edit_btn = item_field.children[item_selector].children[2];
-
-    for(let i = 0; i < item_field.children.length; i++){
-
-        
-        
-        if(item_field.children[i] == edit_btn.parentElement){
-
-            let item_value = edit_btn.parentElement.childNodes[1].textContent;
-            console.log(item_value);
-
-            let edit_field = document.createElement('p');
-            edit_field.appendChild(edit_input);
-
-            let cancel_btn = document.createElement('button');
-            let cancel_text = document.createTextNode('cancel');
-            cancel_btn.appendChild(cancel_text);
-            let save_btn = document.createElement('button');
-            let save_text = document.createTextNode('save');
-            save_btn.appendChild(save_text);
-            edit_field.insertAdjacentElement('beforeend', cancel_btn);
-            edit_field.insertAdjacentElement('beforeend', save_btn);
-            
-            
-            item_field.replaceChild(edit_field, item_field.children[i]);
-            edit_input.value = edit_btn.parentElement.childNodes[1].textContent;
-            
-            
-
-            edit_input.focus();
-
-
-
-
-            cancel_btn.addEventListener('click', cancel_fun);
-            edit_input.addEventListener('blur', blur_fun);
-            function blur_fun(){
-                cancel_fun();
-            }
-            edit_input.addEventListener('keydown', (e) => {
-                if(e.key == "Enter"){
-                    save_fun();
-                }
-            });
-            // save_btn.addEventListener('click', save_fun);
-            save_btn.addEventListener('mousedown', (e) => {
-                if(e.button == 0){
-                    save_fun();
-                }
-            });
-
-
-
-            
-            function cancel_fun(){
-                for(let i = 0; i < item_field.children.length; i++){
-                    if(item_field.children[i] == cancel_btn.parentElement){
-                        item_field.replaceChild(element, item_field.children[i]);
-                    }
-                }
-            }
-
-            function save_fun(){
-                edit_input.removeEventListener('blur', blur_fun);
-                for(let i = 0; i < item_field.children.length; i++){
-                    if(item_field.children[i] == save_btn.parentElement){
-                        
-
-                        item_field.replaceChild(element, item_field.children[i]);
-                        
-                        let new_node = document.createTextNode(edit_input.value);
-                        label.replaceChild(new_node, label.childNodes[0]);
-                        
-                        item_field.children[i].replaceChild(label, item_field.children[i].children[1]);
-
-                        save_data();
-                    }
-                }
             }
         }
     }
@@ -309,6 +190,8 @@ show_data();
 for(let i = 0; i < item_field.children.length; i++){
     item_field.children[i].children[2].classList.add(`edit${i}`);
     if(item_field.children[i].children[2].innerHTML == 'edit'){
-        item_field.children[i].children[2].addEventListener('click', external_edit_fun);
+        item_field.children[i].children[2].addEventListener('click', edit_fun);
     }
 }
+
+item_num = item_field.children.length;
